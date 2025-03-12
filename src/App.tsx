d1976e7648fed1cb140c0c86cd344cc26c99a966
JAVA-BOO <harbayomiamos@gmail.com>
2025-03-12 23:18:29 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="bg-teal-600">Submit</Button>
+    </>
+  );
+}
+
+export default App;
